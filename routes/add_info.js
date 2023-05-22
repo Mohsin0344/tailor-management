501977628f2de43c00f9d6e58e0785bb4deaa331
Mohsin0344 <66850855+Mohsin0344@users.noreply.github.com>
2023-05-22 15:35:50 +0500

    routes/add_info.js

--- routes/add_info.js
+++ routes/add_info.js
@@ -1,19 +1,9 @@
 var express = require("express");
 var router = express.Router();
+var userController = require('../controllers/users')
 
-router.get("/singleuser", (req, res) => {
-    let userId = req.userId;
-    res.send(
-        {
-            "message": "Get single user route of id "  + userId
-        }
-    );
-});
+router.get("/singleuser", userController.get);
 
-router.post("/addSizes", (req, res) => {
-    res.send({
-        "message": "In add sizes route"
-    });
-});
+router.post("/addSizes", userController.addSizes);
 
 module.exports = router;
