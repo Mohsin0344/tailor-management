b37057cc6eb1d2694b2450134a925cb52fedbf86
Mohsin0344 <66850855+Mohsin0344@users.noreply.github.com>
2023-05-22 15:35:20 +0500

    controller/user.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const user = require("../models/user");
+const Sizes = require('../models/sizes');
 
 module.exports = {
     createUser: async (req, res, next) => {
@@ -30,5 +31,78 @@ module.exports = {
                 "message": e.message,
             });
         }
+    },
+
+    get: async (req, res, next) => {
+        const userId = req.userId;
+        try {
+
+            if(!userId) {
+                return res.send({
+                    "message": "user id is required"
+                });
+            }
+
+            const getUser = await user.findOne(
+                {
+                    where: {
+                        id: userId
+                    }
+                }
+            );
+
+            if(!getUser) {
+                return res.send({
+                    "message": "User does not exists"
+                });
+            }
+
+            res.send(
+                {
+                    "message": "user found",
+                    "data": getUser
+                }
+            );
+        } catch(e) {
+            res.status(400).send({
+                "message": e.message,
+            });
+        }
+    },
+
+    addSizes: async (req, res, next) => {
+        const userId = req.userId;
+        const {length, width, fk_size_type_id} = req.body;
+
+         try {
+
+            if(!userId || !length, !width, !fk_size_type_id) {
+                return res.send(
+                    {
+                        "message": "Some data is missing",
+                    }
+                );
+            }
+
+            const sizes = await Sizes.create(
+                {
+                    length,
+                    width,
+                    fk_user_id: userId,
+                    fk_size_type_id
+                }
+            );
+
+            res.send(
+                {
+                    "message": "Size added successfully",
+                    'data': sizes
+                }
+            );
+         } catch(e) {
+            res.status(400).send({
+                "message": e.message,
+            });
+         }
     }
 }
