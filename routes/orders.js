0b4bae66a367c83e52f7af3b1dee0bfd58233b11
Mohsin0344 <66850855+Mohsin0344@users.noreply.github.com>
2023-05-24 10:53:42 +0500

    routes/orders.js

--- routes/orders.js
+++ routes/orders.js
@@ -1,9 +1,13 @@
 const express = require("express");
 var router = express.Router();
 var orderController = require("../controllers/orders")
+var orderController = require('../controllers/orders')
+var orderId = require('../middleware/order_id');
 
-router.get("/details", orderController.details);
+router.get("/:orderId/details", orderId, orderController.details);
 
-router.put("/deliver", orderController.deliver);
+router.put("/:orderId/deliver", orderId, orderController.deliver);
+
+router.post("/placeOrder", orderController.placeOrder);
 
 module.exports = router;
