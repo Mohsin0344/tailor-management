fc23e1a2507a62ea9386a82b3453a3ce092e8c7e
Mohsin0344 <66850855+Mohsin0344@users.noreply.github.com>
2023-05-24 10:53:32 +0500

    routes/add_info.js
    post place order moved to /orders

--- routes/add_info.js
+++ routes/add_info.js
@@ -1,16 +1,12 @@
 var express = require("express");
 var router = express.Router();
 var userController = require('../controllers/users')
-var orderController = require('../controllers/orders')
 var orderRoute = require("./orders")
-var orderId = require('../middleware/order_id');
 
 router.get("/singleuser", userController.get);
 
 router.post("/addSizes", userController.addSizes);
 
-router.post("/placeOrder", orderController.placeOrder);
-
-router.use("/orders/:orderId", orderId, orderRoute);
+router.use("/orders", orderRoute);
 
 module.exports = router;
